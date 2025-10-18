afbcd84ef47aec032b0548ce162b92aa99197aac
nordowl <jonas@nordstudio.digital>
2025-10-19 01:52:01 +0200

    docs: writing...

--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -1,10 +1,20 @@
 import defaultMdxComponents from "fumadocs-ui/mdx"
+import * as TabsComponents from "fumadocs-ui/components/tabs"
+import { Accordion, Accordions } from "fumadocs-ui/components/accordion"
+import { Step, Steps } from "fumadocs-ui/components/steps"
 import type { MDXComponents } from "mdx/types"
+import * as icons from "lucide-react"
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
+    ...(icons as unknown as MDXComponents),
     ...defaultMdxComponents,
+    ...TabsComponents,
+    Accordion,
+    Accordions,
+    Step,
+    Steps,
     ...components,
-  }
+  } satisfies MDXComponents
 }
