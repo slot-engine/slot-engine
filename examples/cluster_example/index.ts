36768a2be68a725dc630b17205cdd4d2ab45d87a
nordowl <jonas@nordstudio.digital>
2025-12-04 19:28:25 +0100

    feat(examples): wip cluster example

---
+++ examples/cluster_example/index.ts
@@ -0,0 +1,302 @@
+import {
+  GameMode,
+  GameSymbol,
+  InferGameType,
+  OptimizationConditions,
+  OptimizationParameters,
+  OptimizationScaling,
+  ResultSet,
+  createSlotGame,
+  defineGameModes,
+  defineSymbols,
+  defineUserState,
+  SPIN_TYPE,
+} from "@slot-engine/core"
+import { GENERATORS } from "./src/reels"
+import { onHandleGameFlow } from "./src/onHandleGameFlow"
+import { maxwinReelsEvaluation } from "./src/evaluations"
+
+export const userState = defineUserState({
+  boardMultis: [] as number[][],
+})
+
+export type UserStateType = typeof userState
+
+export const symbols = defineSymbols({
+  S: new GameSymbol({
+    id: "S",
+    properties: {
+      isScatter: true,
+    },
+  }),
+  W: new GameSymbol({
+    id: "W",
+    pays: {
+      5: 1,
+      6: 1.5,
+      7: 1.75,
+      8: 2,
+      9: 2.5,
+      10: 5,
+      11: 7.5,
+      12: 15,
+      13: 35,
+      14: 70,
+      15: 150,
+    },
+    properties: { isWild: true },
+  }),
+  H1: new GameSymbol({
+    id: "H1",
+    pays: {
+      5: 1,
+      6: 1.5,
+      7: 1.75,
+      8: 2,
+      9: 2.5,
+      10: 5,
+      11: 7.5,
+      12: 15,
+      13: 35,
+      14: 70,
+      15: 150,
+    },
+  }),
+  H2: new GameSymbol({
+    id: "H2",
+    pays: {
+      5: 0.75,
+      6: 1,
+      7: 1.25,
+      8: 1.5,
+      9: 2,
+      10: 4,
+      11: 6,
+      12: 12.5,
+      13: 30,
+      14: 60,
+      15: 100,
+    },
+  }),
+  H3: new GameSymbol({
+    id: "H3",
+    pays: {
+      5: 0.5,
+      6: 0.75,
+      7: 1,
+      8: 1.25,
+      9: 1.5,
+      10: 3,
+      11: 4.5,
+      12: 10,
+      13: 20,
+      14: 40,
+      15: 60,
+    },
+  }),
+  H4: new GameSymbol({
+    id: "H4",
+    pays: {
+      5: 0.4,
+      6: 0.5,
+      7: 0.75,
+      8: 1,
+      9: 1.25,
+      10: 2,
+      11: 3,
+      12: 5,
+      13: 10,
+      14: 20,
+      15: 40,
+    },
+  }),
+  L1: new GameSymbol({
+    id: "L1",
+    pays: {
+      5: 0.3,
+      6: 0.4,
+      7: 0.5,
+      8: 0.75,
+      9: 1,
+      10: 1.5,
+      11: 2.5,
+      12: 3.5,
+      13: 8,
+      14: 15,
+      15: 30,
+    },
+  }),
+  L2: new GameSymbol({
+    id: "L2",
+    pays: {
+      5: 0.25,
+      6: 0.3,
+      7: 0.4,
+      8: 0.5,
+      9: 0.75,
+      10: 1.25,
+      11: 2,
+      12: 3,
+      13: 6,
+      14: 12,
+      15: 25,
+    },
+  }),
+  L3: new GameSymbol({
+    id: "L3",
+    pays: {
+      5: 0.2,
+      6: 0.25,
+      7: 0.3,
+      8: 0.4,
+      9: 0.5,
+      10: 1,
+      11: 1.5,
+      12: 2.5,
+      13: 5,
+      14: 10,
+      15: 20,
+    },
+  }),
+})
+
+export type SymbolsType = typeof symbols
+
+export const gameModes = defineGameModes({
+  base: new GameMode({
+    name: "base",
+    cost: 1,
+    rtp: 0.96,
+    reelsAmount: 7,
+    symbolsPerReel: [7, 7, 7, 7, 7, 7, 7],
+    isBonusBuy: false,
+    reelSets: [...Object.values(GENERATORS)],
+    resultSets: [
+      new ResultSet({
+        criteria: "0",
+        quota: 0.4,
+        multiplier: 0,
+        reelWeights: {
+          [SPIN_TYPE.BASE_GAME]: { base: 1 },
+          [SPIN_TYPE.FREE_SPINS]: { bonus: 1 },
+        },
+      }),
+      new ResultSet({
+        criteria: "basegame",
+        quota: 0.4,
+        reelWeights: {
+          [SPIN_TYPE.BASE_GAME]: { base: 1 },
+          [SPIN_TYPE.FREE_SPINS]: { bonus: 1 },
+        },
+      }),
+      new ResultSet({
+        criteria: "freespins",
+        quota: 0.1,
+        forceFreespins: true,
+        reelWeights: {
+          [SPIN_TYPE.BASE_GAME]: { base: 1 },
+          [SPIN_TYPE.FREE_SPINS]: { bonus: 1 },
+        },
+      }),
+      new ResultSet({
+        criteria: "maxwin",
+        quota: 0.01,
+        forceMaxWin: true,
+        forceFreespins: true,
+        reelWeights: {
+          [SPIN_TYPE.BASE_GAME]: { base: 1 },
+          [SPIN_TYPE.FREE_SPINS]: { bonus: 1 },
+          evaluate: maxwinReelsEvaluation,
+        },
+      }),
+    ],
+  }),
+  bonus: new GameMode({
+    name: "bonus",
+    cost: 100,
+    rtp: 0.96,
+    reelsAmount: 7,
+    symbolsPerReel: [7, 7, 7, 7, 7, 7, 7],
+    isBonusBuy: true,
+    reelSets: [...Object.values(GENERATORS)],
+    resultSets: [
+      new ResultSet({
+        criteria: "freespins",
+        quota: 0.9,
+        forceFreespins: true,
+        reelWeights: {
+          [SPIN_TYPE.BASE_GAME]: { base: 1 },
+          [SPIN_TYPE.FREE_SPINS]: { bonus: 1 },
+        },
+      }),
+      new ResultSet({
+        criteria: "maxwin",
+        quota: 0.01,
+        forceMaxWin: true,
+        forceFreespins: true,
+        reelWeights: {
+          [SPIN_TYPE.BASE_GAME]: { base: 1 },
+          [SPIN_TYPE.FREE_SPINS]: { bonus: 1 },
+          evaluate: maxwinReelsEvaluation,
+        },
+      }),
+    ],
+  }),
+})
+
+export type GameModesType = typeof gameModes
+
+export type GameType = InferGameType<GameModesType, SymbolsType, UserStateType>
+
+export const game = createSlotGame<GameType>({
+  id: "example-02",
+  name: "Example Cluster Game",
+  maxWinX: 5000,
+  gameModes,
+  symbols,
+  padSymbols: 1,
+  scatterToFreespins: {
+    [SPIN_TYPE.BASE_GAME]: {
+      3: 10,
+      4: 12,
+      5: 15,
+      6: 20,
+      7: 30,
+    },
+    [SPIN_TYPE.FREE_SPINS]: {
+      3: 10,
+      4: 12,
+      5: 15,
+      6: 20,
+      7: 30,
+    },
+  },
+  userState,
+  hooks: {
+    onHandleGameFlow,
+  },
+})
+
+game.configureSimulation({
+  simRunsAmount: {
+    base: 10000,
+    bonus: 10000,
+  },
+  concurrency: 8,
+})
+
+game.configureOptimization({
+  gameModes: {},
+})
+
+game.runTasks({
+  doSimulation: true,
+  doOptimization: false,
+  optimizationOpts: {
+    gameModes: ["base", "bonus"],
+  },
+  doAnalysis: true,
+  analysisOpts: {
+    gameModes: ["base", "bonus"],
+  },
+})
---
+++ examples/cluster_example/src/onHandleGameFlow.ts
@@ -0,0 +1,237 @@
+import {
+  ClusterWinType,
+  WinCombination,
+  GameContext,
+  GameSymbol,
+  Reels,
+  SPIN_TYPE,
+} from "@slot-engine/core"
+import { GameModesType, SymbolsType, UserStateType } from ".."
+
+type Context = GameContext<GameModesType, SymbolsType, UserStateType>
+
+export function onHandleGameFlow(ctx: Context) {
+  makeInitialBoardMultis(ctx)
+  drawBoard(ctx)
+  handleAnticipation(ctx)
+  handleTumbles(ctx)
+  ctx.services.wallet.confirmSpinWin()
+  checkFreespins(ctx)
+}
+
+function drawBoard(ctx: Context) {
+  const reels = ctx.services.board.getRandomReelset()
+  const scatter = ctx.config.symbols.get("S")!
+
+  const numScatters = Number(
+    ctx.services.rng.weightedRandom(
+      getScatterWeights(ctx.state.currentResultSet.criteria),
+    ),
+  )
+
+  if (
+    // If free spins are forced via the result set, draw board with scatters
+    ctx.state.currentResultSet.forceFreespins &&
+    ctx.state.currentSpinType == SPIN_TYPE.BASE_GAME
+  ) {
+    while (true) {
+      ctx.services.board.resetBoard()
+
+      const reelStops = ctx.services.board.getReelStopsForSymbol(reels, scatter)
+      const scatterReelStops = ctx.services.board.getRandomReelStops(
+        reels,
+        reelStops,
+        numScatters,
+      )
+
+      ctx.services.board.drawBoardWithForcedStops({
+        reels,
+        forcedStops: scatterReelStops,
+      })
+
+      const scatInvalid = ctx.services.board.isSymbolOnAnyReelMultipleTimes(scatter)
+      const [scatCount] = ctx.services.board.countSymbolsOnBoard(scatter)
+
+      if (scatCount == numScatters && !scatInvalid) break
+    }
+  } else if (
+    // If spin should NOT trigger free spins, draw board with up to 2 scatters
+    !ctx.state.currentResultSet.forceFreespins &&
+    ctx.state.currentSpinType == SPIN_TYPE.BASE_GAME
+  ) {
+    while (true) {
+      ctx.services.board.resetBoard()
+      ctx.services.board.drawBoardWithRandomStops(reels)
+
+      const scatInvalid = ctx.services.board.isSymbolOnAnyReelMultipleTimes(scatter)
+      const [scatCount] = ctx.services.board.countSymbolsOnBoard(scatter)
+
+      if (scatCount > ctx.config.anticipationTriggers[ctx.state.currentSpinType]) {
+        continue
+      }
+
+      if (!scatInvalid) break
+    }
+  } else {
+    drawDefaultBoard(ctx, reels, scatter)
+  }
+}
+
+function handleAnticipation(ctx: Context) {
+  const scatter = ctx.config.symbols.get("S")!
+  const [_, scatCount] = ctx.services.board.countSymbolsOnBoard(scatter)
+
+  let count = 0
+
+  for (const [i, reel] of ctx.services.board.getBoardReels().entries()) {
+    if (count >= ctx.config.anticipationTriggers[ctx.state.currentSpinType]) {
+      ctx.services.board.setAnticipationForReel(i, true)
+    }
+    if (scatCount[i] > 0) {
+      count++
+    }
+  }
+}
+
+function handleTumbles(ctx: Context) {
+  const cluster = new ClusterWinType({
+    ctx,
+    wildSymbol: { isWild: true },
+  })
+
+  while (true) {
+    const { payout, winCombinations } = cluster
+      .evaluateWins(ctx.services.board.getBoardReels())
+      .postProcess((wins) => processWins(wins, ctx))
+      .getWins()
+
+    if (payout === 0) break
+
+    const symbolsToRemove = winCombinations.flatMap((wc) =>
+      wc.symbols.map((s) => ({ reelIdx: s.reelIndex, rowIdx: s.posIndex })),
+    )
+
+    ctx.services.wallet.addTumbleWin(payout)
+
+    for (const syms of symbolsToRemove) {
+      const currentMulti = ctx.state.userData.boardMultis[syms.reelIdx][syms.rowIdx]
+      const newMulti = Math.max(1, Math.min(currentMulti * 2, 128))
+      ctx.state.userData.boardMultis[syms.reelIdx][syms.rowIdx] = newMulti
+    }
+
+    ctx.services.board.tumbleBoard(symbolsToRemove)
+  }
+}
+
+function checkFreespins(ctx: Context) {
+  const scatter = ctx.config.symbols.get("S")!
+  const [scatCount] = ctx.services.board.countSymbolsOnBoard(scatter)
+
+  const freespinsAwarded = ctx.services.game.getFreeSpinsForScatters(
+    ctx.state.currentSpinType,
+    scatCount,
+  )
+
+  // no freespins, return early
+  if (freespinsAwarded <= 0) return
+
+  ctx.services.game.awardFreespins(freespinsAwarded)
+
+  // Ensure we only trigger free spins from base game.
+  // Our playFreeSpins function handles the free spins loop already and we don't want recursion.
+  if (ctx.state.currentSpinType == SPIN_TYPE.BASE_GAME) {
+    ctx.services.data.recordSymbolOccurrence({
+      kind: scatCount,
+      symbolId: scatter.id,
+      spinType: ctx.state.currentSpinType,
+    })
+
+    ctx.services.data.record({
+      triggeredFS: true,
+    })
+
+    playFreeSpins(ctx)
+  }
+}
+
+function playFreeSpins(ctx: Context) {
+  ctx.state.currentSpinType = SPIN_TYPE.FREE_SPINS
+  while (ctx.state.currentFreespinAmount > 0) {
+    ctx.state.currentFreespinAmount--
+    drawBoard(ctx)
+    handleAnticipation(ctx)
+    handleTumbles(ctx)
+    ctx.services.wallet.confirmSpinWin()
+    checkFreespins(ctx)
+  }
+}
+
+function getScatterWeights(key: string) {
+  const SCATTER_WEIGHTS = {
+    freespins: {
+      3: 80,
+      4: 10,
+      5: 1,
+      6: 0.5,
+      7: 0.1,
+    },
+    maxwin: {
+      6: 1,
+      7: 2,
+    },
+  }
+
+  if (key in SCATTER_WEIGHTS) {
+    return SCATTER_WEIGHTS[key as keyof typeof SCATTER_WEIGHTS]
+  }
+
+  return SCATTER_WEIGHTS.freespins
+}
+
+function drawDefaultBoard(ctx: Context, reels: Reels, scatter: GameSymbol) {
+  while (true) {
+    ctx.services.board.resetBoard()
+    ctx.services.board.drawBoardWithRandomStops(reels)
+
+    const scatInvalid = ctx.services.board.isSymbolOnAnyReelMultipleTimes(scatter)
+
+    if (!scatInvalid) break
+  }
+}
+
+function makeInitialBoardMultis(ctx: Context) {
+  const mode =
+    ctx.config.gameModes[ctx.state.currentGameMode as keyof typeof ctx.config.gameModes]
+
+  const reelsNum = mode.reelsAmount
+  const symbolsPerReel = mode.symbolsPerReel
+
+  const boardMultis: number[][] = []
+
+  for (let r = 0; r < reelsNum; r++) {
+    const reelMultis: number[] = []
+    for (let s = 0; s < symbolsPerReel[r]; s++) {
+      reelMultis.push(0)
+    }
+    boardMultis.push(reelMultis)
+  }
+
+  ctx.state.userData.boardMultis = boardMultis
+}
+
+function processWins(wins: WinCombination[], ctx: Context) {
+  const winCombinations = wins.map((wc) => {
+    const multiForCluster = wc.symbols.reduce((multi, s) => {
+      const multiOnPos = ctx.state.userData.boardMultis[s.reelIndex][s.posIndex]
+      return multiOnPos >= 2 ? multiOnPos + multi : multi
+    }, 0)
+
+    const payout = wc.payout * Math.max(1, multiForCluster)
+
+    return {
+      ...wc,
+      payout,
+    }
+  })
+  return { winCombinations }
+}
