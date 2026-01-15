edb90a75defdd70133579fa7deee6021851c7649
nordowl <jonas@nordstudio.digital>
2026-01-15 19:24:33 +0100

    refactor(examples): rename static reels

--- examples/cluster_example/src/reels.ts
+++ examples/cluster_example/src/reels.ts
@@ -4,14 +4,14 @@ import path from "path"
 export const GENERATORS = {
   base: new StaticReelSet({
     id: "base",
-    csvPath: path.join(__dirname, "../static-reels/base.csv"),
+    csvPath: path.join(__dirname, "../static-reels/reels_base.csv"),
   }),
   bonus: new StaticReelSet({
     id: "bonus",
-    csvPath: path.join(__dirname, "../static-reels/bonus.csv"),
+    csvPath: path.join(__dirname, "../static-reels/reels_bonus.csv"),
   }),
   maxwin: new StaticReelSet({
     id: "maxwin",
-    csvPath: path.join(__dirname, "../static-reels/maxwin.csv"),
+    csvPath: path.join(__dirname, "../static-reels/reels_maxwin.csv"),
   }),
 } as const
